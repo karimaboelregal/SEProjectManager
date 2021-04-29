1e76328b0980f57016ec2f55e9c4e3afc845c34a
kikosvg <karim1809252@miuegypt.edu.eg>
2021-04-30 01:03:07 +0200

    Login, sidebar

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.sidebar')
+@extends('layouts.app')
+@section('content2')
+<script>
+var element = document.getElementById("courses");
+element.classList.add("show");
+</script>
+@endsection
+
