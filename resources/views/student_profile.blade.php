a635340f9e1ceb2645673bbed391697c877a2e0e
kikosvg <karim1809252@miuegypt.edu.eg>
2021-05-03 16:24:21 +0200

    edited margins

--- resources/views/student_profile.blade.php
+++ resources/views/student_profile.blade.php
@@ -27,7 +27,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-12 text-center">
                 <div class="profile-head">
                     <h5>
                         Maria antoinette
@@ -51,7 +51,7 @@
         </div>
         <div class="row">
             
-            <div class="col-md-8">
+            <div class="col-md-12 text-center">
                 <div class="tab-content profile-tab" id="myTabContent">
                     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                         <div class="row">
