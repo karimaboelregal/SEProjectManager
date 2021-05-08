a41c30183c60f8c071ef341a93ff19d6d6407eb7
ahmed serag <ahmedserag2000@gmail.com>
2021-05-08 16:12:36 +0200

    fixed home cards

--- resources/views/student_home.blade.php
+++ resources/views/student_home.blade.php
@@ -49,22 +49,32 @@
         </div>
 
 </div>
-<div style="margin-top:30px;max-width:100%;" class="row  justify-content-center ">
-     <div class="col">
-      <div class="card" style="width: 18rem;max-height:40%">
-        <img src="images/team.png" class="card-img-top" alt="">
+<div class="row" style = "margin:30px;" >
+    
+    <div class="col-sm">
+      <div class="card" style="width:100%;">
+
+            <img src="images/team.png" class="card-img-top" style="width:50%;" alt="">
+
           <div class="card-body">
              <h5 class="card-title">Myteam</h5>
             <p class="card-text">Checkout the new content and keep yourself posted to your team.</p>
-          <a href="student_team" class="btn btn-primary">Open Team</a>
-      </div>  
-      <div class="col">
-     <div class="card" style="width: 18rem;">
-        <img src="images/profile.png" class="card-img-top" alt="">
+          <a href="student_team" class="btn btn-danger">Open Team</a>
+          </div>
+       </div>
+    </div>  
+    <div class="col-sm"></div>
+
+    <div class="col-sm">
+     <div class="card">
+
+
+        <img src="images/profile.png" class="card-img-top" style="width:50%;height:50%;" alt="">
+
           <div class="card-body">
              <h5 class="card-title">Profile</h5>
             <p class="card-text">Click to Edit your Bio and Prefrences in your profile.</p>
-          <a href="student_profile" class="btn btn-primary">Open profile</a>
+          <a href="student_profile" class="btn btn-danger">Open profile</a>
     </div>
 </div>
 </div>
