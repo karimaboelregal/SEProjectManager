1e485fd7f4e66e330aafcfb53f943f1cb8a46716
kikosvg <karim1809252@miuegypt.edu.eg>
2021-05-08 03:43:49 +0200

    edit user form done

--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -4,21 +4,59 @@
 var element = document.getElementById("users");
 element.classList.add("show");
 </script>
-<div style="margin-top:30px;max-width:100%;" class="row d-flex justify-content-center ">
-<div style="width:600px; height:450px;border-radius:5px;border:1px solid #C63E47;margin-top:50px;">
-<a style="font-size:20px;margin-left:40px;margin-top:30px;">Name:</a> <input style="width:75%;margin-left:20px;margin-top:30px;" type="text" class="inputDesign" name="name" placeholder="karim mohamed"><br>
-<a style="font-size:20px;margin-left:40px;margin-top:30px;">Email:</a> <input style="width:75%;margin-left:20px;margin-top:30px;" type="text" class="inputDesign" name="name" placeholder="karim@gmail.com"><br>
-<a style="font-size:20px;margin-left:40px;margin-top:30px;">Email:</a> 
-<div class="custom-control custom-checkbox">  
-    <a style="font-size:20px;">Courses: </a>
-    <input style="margin-left:30px;margin-top:5px;"type="checkbox" class="custom-control-input" id="customCheck1">  
-    <label style="margin-left:30px;margin-top:5px;" class="custom-control-label" for="customCheck1" style="font-size:15px">Remember me</label>  
-</div>  
+
+<div class="content d-flex justify-content-center" >
+
+                        <div class="card w-50">
+                            <div class="card-header" style="background-color:#C63E47;color:White"><strong>Edit user</strong><small> Form</small></div>
+                            <div class="card-body card-block">
+                            <form action="{{route('createNewProject')}}" method="post">
+                                        {{csrf_field()}}
+                                        <div class="form-group"><label for="name" class=" form-control-label">Name</label>
+                                            <input type="text" id="project_title" name="project_title" placeholder="karim mohamed" class="form-control"></div>
+                                        <div class="form-group"><label for="description" class=" form-control-label">Email</label>
+                                            <input type="text" id="desc" name="description" placeholder="karim@gmail.com" class="form-control"></div>
+                                        <div class="form-group"><label for="description" class=" form-control-label">Courses</label>
+                                            <input type="text" id="desc" name="client_number" placeholder="SE,HCI" class="form-control"></div>
+                                        <div class="form-group">
+                                        <label for="code" class=" form-control-label">Select Role</label>
+                                            <select class="form-control" name="team_id">
+                                                <option>Admin</option>
+                                                <option>Professor</option>
+                                                <option>Student</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                        <label for="code" class=" form-control-label">Courses</label>
+                                        <div class="custom-control form-control-lg custom-checkbox" style="margin-top:-15px">  
+                                        <input type="checkbox" class="custom-control-input" id="customCheck1">  
+                                        <label class="custom-control-label" for="customCheck1" style="font-size:15px">SE</label>  
+                                        </div>  
+                                        <div class="custom-control form-control-lg custom-checkbox" style="margin-top:-15px">  
+                                        <input type="checkbox" class="custom-control-input" id="asd">  
+                                        <label class="custom-control-label" for="asd" style="font-size:15px">HCI</label>  
+                                        </div>  
+
+                                        </div>
+                                    </div>
+                                <div class="modal-footer">
+                                    <div class="form-group">
+                                        <button onclick="location.href = '{{route('index')}}'" type="button" class="btn btn-outline" data-dismiss="modal">Close</button>
+                                        <button class="btn btn-outline" type="submit" value="submit">submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+
+                
 
 
-</div> 
-</div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+
 
 
 @endsection
-
