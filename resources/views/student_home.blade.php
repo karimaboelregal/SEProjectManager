3661924e5a064be6a2d8cd961a5666e4123d42a7
ahmed serag <ahmedserag2000@gmail.com>
2021-05-08 16:26:48 +0200

    fixed some ui issues in home

--- resources/views/student_home.blade.php
+++ resources/views/student_home.blade.php
@@ -15,10 +15,50 @@
     
     <div style="margin-top:20px;max-width:100%;" class="row d-flex justify-content-center" >
     
-     <img src='images/home3.jpg' width=90% height=500>
+     <img src='images/lms.jpg' width=90% height=500>
        
     </div>
-    <div style="margin-top:30px;max-width:100%;" class="row d-flex justify-content-center ">
+
+    <div class="row" style="margin:30px;">
+
+        <div class="col-sm">
+            <div class="card" style="width:100%;">
+
+                <img src="images/team.png" class="card-img-top" style="width:50%;" alt="">
+
+                <div class="card-body">
+                    <h5 class="card-title">Myteam</h5>
+                    <p class="card-text">Checkout the new content and keep yourself posted to your team.</p>
+                    <a href="student_team" class="btn btn-danger">Open Team</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm"></div>
+
+        <div class="col-sm">
+            <div class="card">
+
+
+                <img src="images/profile.png" class="card-img-top" style="width:50%;height:50%;" alt="">
+
+                <div class="card-body">
+                    <h5 class="card-title">Profile</h5>
+                    <p class="card-text">Click to Edit your Bio and Prefrences in your profile.</p>
+                    <a href="student_profile" class="btn btn-danger">Open profile</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+        <h3 style="margin:15px;">
+            Your Courses
+        </h3>
+
+    
+    
+
+    <div style="margin-top:30px;margin-bottom:30px;max-width:100%;" class="row d-flex justify-content-center ">
+    
         <div class="turningButtonContainer">
             <div class="turningButtonContainerInner">
                 <div class="turningButton"><i style="font-size:35px;margin-top:55px;"class="icons far fa-dot-circle"></i><span>SE</span></div>
@@ -49,35 +89,8 @@
         </div>
 
 </div>
-<div class="row" style = "margin:30px;" >
-    
-    <div class="col-sm">
-      <div class="card" style="width:100%;">
-
-            <img src="images/team.png" class="card-img-top" style="width:50%;" alt="">
-
-          <div class="card-body">
-             <h5 class="card-title">Myteam</h5>
-            <p class="card-text">Checkout the new content and keep yourself posted to your team.</p>
-          <a href="student_team" class="btn btn-danger">Open Team</a>
-          </div>
-       </div>
-    </div>  
-    <div class="col-sm"></div>
-
-    <div class="col-sm">
-     <div class="card">
 
 
-        <img src="images/profile.png" class="card-img-top" style="width:50%;height:50%;" alt="">
-
-          <div class="card-body">
-             <h5 class="card-title">Profile</h5>
-            <p class="card-text">Click to Edit your Bio and Prefrences in your profile.</p>
-          <a href="student_profile" class="btn btn-danger">Open profile</a>
-    </div>
-</div>
-</div>
 
 
 
