d1026edbd9fc4e460f045a2cdfa90f87c5034508
ahmed serag <ahmedserag2000@gmail.com>
2021-06-09 16:10:24 +0200

    added gpa skills image and phone in the users table

---
+++ database/migrations/2021_06_09_140258_update_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            
+            $table->string('GPA','10')->nullable();
+            $table->string('Skills','100')->nullable();
+            $table->string('Image','100')->nullable();
+            $table->string('Phone','100')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
