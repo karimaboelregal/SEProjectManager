758685ede74874c5fcf20ba06435f62f7425bd0b
kikosvg <karim1809252@miuegypt.edu.eg>
2021-06-13 14:29:57 +0200

    fixed a bug

--- resources/views/layouts/student_topbar.blade.php
+++ resources/views/layouts/student_topbar.blade.php
@@ -144,7 +144,7 @@ $user = Session::get("userData");
 if ($state != 1 && Request::path() != "login") {
     echo "<script>window.location.href='login'</script>";
 } elseif ($state == 1 && $user->Name == "Student" && (!Str::contains(Request::path(), 'student_') || !Str::contains(Request::path(), 'ViewProject'))) {
-    //echo "<script>window.location.href='/student_home'</script>";
+    echo "<script>window.location.href='/student_home'</script>";
 }
 @endphp
 
