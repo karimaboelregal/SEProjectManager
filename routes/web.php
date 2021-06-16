b19a1bfda916c5feaa573ce802261c246d1ae80e
ahmed serag <ahmedserag2000@gmail.com>
2021-06-16 03:37:49 +0200

    student submits into discussion

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\SurveyController;
 use App\Http\Controllers\loginControl;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\InvitationController;
-
+use App\Http\Controllers\DiscussionController;
 
 
 /*
@@ -89,7 +89,7 @@ Route::get('/ViewSurvey/{id}', [SurveyController::class,'ViewSurvey'])->name('Vi
 Route::get('/ViewSurveyInsights/{id}', [SurveyController::class,'ViewSurveyInsights'])->name('ViewSurveyInsights');
 Route::post('/InsertSurvey', [SurveyController::class,'InsertSurvey'])->name('InsertSurvey');
 
-
+Route::post('/store', [DiscussionController::class,'store'])->name('store');
 
 Route::get('/courses', [CoursesController::class,'index'])->name('index');
 
