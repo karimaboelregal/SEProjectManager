c645ebb195d24d0bde2bf3a4d8110108ff6c456e
ahmed serag <ahmedserag2000@gmail.com>
2021-06-20 01:39:15 +0200

    added some form validation and fixed bug in update home

--- resources/views/createProjectForm.blade.php
+++ resources/views/createProjectForm.blade.php
@@ -9,18 +9,18 @@
                             <form action="{{route('createNewProject')}}" method="post">
                                         {{csrf_field()}}
                                         <div class="form-group"><label for="name" class=" form-control-label">Project title</label>
-                                            <input type="text" id="project_title" name="project_title" placeholder="Enter project name" class="form-control"></div>
+                                            <input type="text" id="project_title" name="project_title" placeholder="Enter project name" class="form-control" required></div>
                                         <div class="form-group"><label for="description" class=" form-control-label">Description</label>
-                                            <input type="text" id="desc" name="description" placeholder="Enter description" class="form-control"></div>
+                                            <input type="text" id="desc" name="description" placeholder="Enter description" class="form-control" required></div>
                                         <div class="form-group"><label for="description" class=" form-control-label">Client number</label>
-                                            <input type="text" id="desc" name="client_number" placeholder="Enter client number" class="form-control"></div>
+                                            <input type="text" id="desc" name="client_number" placeholder="Enter client number" class="form-control" required></div>
                                         <div class="form-group"><label for="description" class=" form-control-label">Client name</label>
-                                            <input type="text" id="desc" name="client_name" placeholder="Enter client name" class="form-control"></div>
+                                            <input type="text" id="desc" name="client_name" placeholder="Enter client name" class="form-control" required></div>
                                         <div class="form-group"><label for="description" class=" form-control-label">Client Email</label>
-                                            <input type="text" id="desc" name="client_email" placeholder="Enter client email" class="form-control"></div>
+                                            <input type="text" id="desc" name="client_email" placeholder="Enter client email" class="form-control" required></div>
                                         <div class="form-group">
                                         <label for="code" class=" form-control-label">Select team</label>
-                                            <select class="form-control" name="team_id">
+                                            <select class="form-control" name="team_id" required>
                                                 @foreach($teams as $team)
                                                 <option value="{{$team->id}}">{{$team->Name}}</option>
                                                 @endforeach
@@ -29,7 +29,7 @@
                                         <div class="form-group">
 
                                         <label for="code" class=" form-control-label">Select project template</label>
-                                            <select class="form-control" name="tempID">
+                                            <select class="form-control" name="tempID" required>
                                             
                                                 @foreach($project_templates as $temp)
 
@@ -40,7 +40,7 @@
                                     </div>
                                 <div class="modal-footer">
                                     <div class="form-group">
-                                        <button onclick="location.href = '{{route('index')}}'" type="button" class="btn btn-outline" data-dismiss="modal">Close</button>
+                                        <button onclick="location.href = '{{URL::previous()}}'" type="button" class="btn btn-outline" data-dismiss="modal">Back</button>
                                         <button class="btn btn-outline" type="submit" value="submit">submit</button>
                                     </div>
                                 </div>
